6ddc9f94477cfdf6d9d748f64621963c49957601
Saiful Islam <saiful.bd.developer@gmail.com>
2022-07-29 00:23:57 +0600

    route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Facebook from "./pages/Facebook/Facebook";
+import Facebook from "./pages/Facebook";
 import Instagram from "./pages/Instagram/Instagram";
 
 import PageNotFound from "./pages/notfound";
